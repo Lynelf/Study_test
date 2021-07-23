0ef0ec6e814092c0d66a4d9671f3aba91228e824
Lynelf <1732819459@qq.com>
2021-07-23 23:19:40 +0800

    gaile

--- abcd.py
+++ abcd.py
@@ -1,4 +1,27 @@
 from typing import AbstractSet
 
+'''基本的数据类型
+#print(878979)
+#print("hello world")
 
-print(878979)
+print("哈哈哈"+"为什么")
+print("a"*5)
+
+print(1+2)
+print(2>3)
+快捷注释 ctrl + /
+
+'''
+
+
+# a = input("请输入内容1：")
+# b = input("请输入内容2：")
+# print("输入的内容1的类型是",type(a))
+# a = float(a)
+# b = float(b)
+# print("获取的内容：",a+b)
+
+c = "true167"
+print(type(c))
+d = bool(c)
+print(type(d))
