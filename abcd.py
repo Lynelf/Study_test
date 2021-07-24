5e12e56e46bc084fa740f6d0d33c5aa08bf8cb7d
Lynelf <1732819459@qq.com>
2021-07-24 22:57:11 +0800

    元组数组字典

--- abcd.py
+++ abcd.py
@@ -1,4 +1,4 @@
-from typing import AbstractSet
+from typing import AbstractSet, Hashable
 
 '''基本的数据类型
 #print(878979)
@@ -21,7 +21,44 @@ print(2>3)
 # b = float(b)
 # print("获取的内容：",a+b)
 
-c = "true167"
-print(type(c))
-d = bool(c)
-print(type(d))
+# c = "true167"
+# print(type(c))
+# d = bool(c)
+# print(type(d))
+
+# xx = input("请输入：")
+# print(bool(type(xx) == str))
+
+# a = "a"
+# print(bool(type(a) == int))
+
+
+# 元组
+# htuple = (1,"haha","啦啦","lala","333","haha")
+# print(htuple.count("haha"))
+# print(htuple.index("啦啦"))
+
+
+# 数组
+# hlist = ["keyi","阿里里","aaa","魅力",3]
+# a = hlist.append("lov")
+# print(hlist)
+# b = hlist.count("keyi")
+# print(b)
+# c = hlist.index(3)
+# print(c)
+# d = hlist.insert(3,"漂亮")
+# print(hlist)
+# e = hlist.pop(2) #取走
+# print("取走的值为：",e)
+# f = hlist.remove("阿里里")
+# print(hlist)
+
+
+# 字典
+hdict = {"name":"张三","high":183,"sex":"男"}
+print(hdict["high"])
+hdict["age"] = 19
+print(hdict)
+
+
