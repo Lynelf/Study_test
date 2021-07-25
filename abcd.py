442a8ccd0bf3bf9d4d52ac993377b5a5324aeefc
Lynelf <1732819459@qq.com>
2021-07-25 21:17:21 +0800

    判断 循环

--- abcd.py
+++ abcd.py
@@ -1,4 +1,6 @@
+from sys import stderr
 from typing import AbstractSet, Hashable
+import time
 
 '''基本的数据类型
 #print(878979)
@@ -13,7 +15,7 @@ print(2>3)
 
 '''
 
-
+# pythons数据类型-------------------------------------
 # a = input("请输入内容1：")
 # b = input("请输入内容2：")
 # print("输入的内容1的类型是",type(a))
@@ -33,13 +35,13 @@ print(2>3)
 # print(bool(type(a) == int))
 
 
-# 元组
+# 元组--------------------------------------------------------
 # htuple = (1,"haha","啦啦","lala","333","haha")
 # print(htuple.count("haha"))
 # print(htuple.index("啦啦"))
 
 
-# 数组
+# 数组---------------------------------------------------
 # hlist = ["keyi","阿里里","aaa","魅力",3]
 # a = hlist.append("lov")
 # print(hlist)
@@ -55,10 +57,50 @@ print(2>3)
 # print(hlist)
 
 
-# 字典
-hdict = {"name":"张三","high":183,"sex":"男"}
-print(hdict["high"])
-hdict["age"] = 19
-print(hdict)
+# 字典------------------------------------------------
+# hdict = {"name":"张三","high":183,"sex":"男"}
+# print(hdict["high"])
+# hdict["age"] = 19
+# print(hdict)
 
 
+# for循环，遍历----------------------------------------
+# hdict = {
+#     "name":"zhangsan",
+#     "age":34
+# }
+
+# for i in hdict:
+#     print(i)
+#     print(hdict[i])
+#     # print("这是获取到的值",hdict.get(i))
+
+# for i in range(0,3):
+#     print(i)
+
+
+# for循环例子-------------------------------------------------
+# hlist  = []
+# llist = []
+# hahalist = [23,45,67,55,77,89,32,12,98,76,88,90,3.9,21]
+# for i in hahalist:
+#     if i >= 60:
+#         hlist.append(i)
+#     else:
+#         llist.append(i)
+# print("大于等于60的数组是",hlist)
+# print("小于60的数组是",llist)
+
+
+# while循环例子---------------------------------------------
+# a = 0
+# while a <  10:
+#     print("a的值",a)
+#     a = a +1
+
+
+adict = {"红灯":30,"绿灯":15,"黄灯":3}
+for i in adict:
+    for j in range(adict[i]):
+        print("{}已经运行{}秒了".format(i,j))
+        time.sleep(1)
